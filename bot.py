6273ba9953a14242d5ccf2f9fa30fc6bc8320e3b
Yuriy <batzotov@gmail.com>
2020-07-09 02:37:55 +0300

    change message.id

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    if message.channel == int(os.environ.get('WORK_CHANNEL')):
+    if message.channel.id == int(os.environ.get('WORK_CHANNEL')):
         await bot.process_commands(message)
 
 
